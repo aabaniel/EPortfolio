a1e05e36130c3c765c12cd99c9b5b1fff658c0e6
A <140327394+aabaniel@users.noreply.github.com>
2026-09-06 16:33:34 +0800

    Fix image source path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -85,7 +85,7 @@ function App() {
       <section className="hero shell">
         <div className="profile-frame">
           <div className="profile-placeholder">
-            <img src="/src/pic.png" alt="Aaron Abaniel"/>
+            <img src="./src/pic.png" alt="Aaron Abaniel"/>
           </div>
         </div>
 
